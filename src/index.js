e470e3634a28f36360376abe7eba464c09f27206
Camille Duprez <launchmesomegames@gmail.com>
2023-07-18 18:11:21 +0200

    Page error + modifications styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as RouterProvider, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Lodging from './pages/Lodging';
@@ -9,13 +9,14 @@ import Error from './pages/Error';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider>
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />} errorElement={<Error />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/lodging/:id" element={<Lodging />} />
+        <Route path="*" element={<Error />} />
       </Routes>
-    </RouterProvider>
+    </Router>
   </React.StrictMode>
 )
 
