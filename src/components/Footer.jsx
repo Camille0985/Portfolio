cc50d8964ebd3195976f7c515e6b6e3b3349b8b6
Camille Duprez <launchmesomegames@gmail.com>
2023-10-03 14:54:17 +0200

    affichage en responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Footer = () => {
 
+    const handleClick = () => {
+        window.open("https://github.com/Camille0985", "_blank");
+    };
 
     return (
         <footer id="Footer">
+            <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="Logo" onClick={handleClick} className="logo-git"/>
             <a href="mailto:duprez_camille@yahoo.com" className='contact' id='Contact'>
                 Me contacter par mail
             </a>
